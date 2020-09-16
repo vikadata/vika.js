249a9d3d687fe7cdf15f70e84c760561fbadd194
Bieber <chenjiabei@vikadata.com>
2020-09-16 21:38:42 +0800

    chore: change developer token to api token

--- lib/vika.ts
+++ lib/vika.ts
@@ -10,7 +10,7 @@ export class Vika {
 
   constructor(public config?: IAuthConfig) {
     if (!config) {
-      throw new Error('请传入 Developer Token。(登陆 vika.cn 在左下角用户中心获取)');
+      throw new Error('请传入 API Token。(登陆 vika.cn 在左下角用户中心获取)');
     }
 
     this.request = new Request(config);
