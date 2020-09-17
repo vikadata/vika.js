1cb8a7d787c6ff44909db8dd7f2eae90ea35596f
Bieber <chenjiabei@vikadata.com>
2020-09-17 14:22:55 +0800

    fix: make class vika to static

--- lib/vika.ts
+++ lib/vika.ts
@@ -2,21 +2,20 @@ import { Datasheet } from './datasheet';
 import { Request, IAuthConfig } from './request';
 
 export class Vika {
+  static config: IAuthConfig;
+  static request: Request;
+
   static auth(config: IAuthConfig) {
-    return new Vika(config);
-  }
-
-  request: Request;
-
-  constructor(public config?: IAuthConfig) {
     if (!config) {
       throw new Error('请传入 API Token。(登陆 vika.cn 在左下角用户中心获取)');
     }
 
+    this.config = config;
     this.request = new Request(config);
+    return Vika;
   }
 
-  datasheet(datasheetId: string) {
+  static datasheet(datasheetId: string) {
     if (!datasheetId) {
       throw new Error('请传入维格表Id, 可以从维格表 url 上获取，维格表 id 通常以 dst 开头');
     }
